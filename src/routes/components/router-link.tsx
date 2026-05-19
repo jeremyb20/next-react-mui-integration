0daef13ee516cfa01dde811944f321203b9d8ef4
Jeremy-Bacca <jbaccab@ucenfotec.ac.cr>
2026-05-19 13:24:50 -0600

    Update router-link.tsx

--- src/routes/components/router-link.tsx
+++ src/routes/components/router-link.tsx
@@ -1,10 +1,28 @@
 import { forwardRef } from 'react';
 import Link, { LinkProps } from 'next/link';
+import { fallbackLng } from '@/app/i18n/settings'; // Ajusta la ruta según tu estructura
 
 // ----------------------------------------------------------------------
 
-const RouterLink = forwardRef<HTMLAnchorElement, LinkProps>(
-  ({ ...other }, ref) => <Link ref={ref} {...other} />
+interface RouterLinkProps extends LinkProps {
+  lng?: string;
+  href: string;
+  children?: React.ReactNode;
+}
+
+const RouterLink = forwardRef<HTMLAnchorElement, RouterLinkProps>(
+  ({ lng = fallbackLng, href, children, ...other }, ref) => {
+    // Construir la URL con el idioma
+    const localizedHref = `/${lng}${href.startsWith('/') ? href : `/${href}`}`;
+
+    return (
+      <Link ref={ref} href={localizedHref} {...other}>
+        {children}
+      </Link>
+    );
+  }
 );
+
+RouterLink.displayName = 'RouterLink';
 
 export default RouterLink;
